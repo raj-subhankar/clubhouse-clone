8928a1b3a31c99b07693999e2db58d0bf22be14d
rj-subhankar <subhankar.rj@gmail.com>
2021-05-14 17:50:24 +0530

    Build PhoneConfirmation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Welcome from './pages/Welcome';
 import PlanLayout from './Layouts/PlanLayout';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import PhoneConfirmation from './pages/PhoneConfirmation';
 
 function App() {
   return (
     <BrowserRouter>
       <PlanLayout>
-            <Welcome />
+        <Route exact path="/" component={Welcome} />
+        <Route exact path="/invite" component={PhoneConfirmation} />
       </PlanLayout>
     </BrowserRouter>
   );
