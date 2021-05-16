896fc203821a2e306d09887da0ee1bb85ecc3e2b
rj-subhankar <subhankar.rj@gmail.com>
2021-05-16 21:21:07 +0530

    Create confirmation screen

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Welcome from './pages/Welcome';
 import PlanLayout from './Layouts/PlanLayout';
 import { BrowserRouter, Route } from 'react-router-dom';
 import PhoneConfirmation from './pages/PhoneConfirmation';
+import CodeConfirm from './pages/CodeConfirm';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <PlanLayout>
         <Route exact path="/" component={Welcome} />
         <Route exact path="/invite" component={PhoneConfirmation} />
+        <Route exact path="/code_confirm" component={CodeConfirm} />
       </PlanLayout>
     </BrowserRouter>
   );
