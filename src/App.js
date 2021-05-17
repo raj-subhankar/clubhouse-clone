78c9eaca22fc9cb5e0d4697c504e02d230bc446f
rj-subhankar <subhankar.rj@gmail.com>
2021-05-17 18:57:26 +0530

    Create Home screen

--- src/App.js
+++ src/App.js
@@ -2,20 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 import Welcome from './pages/Welcome';
 import PlanLayout from './Layouts/PlanLayout';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PhoneConfirmation from './pages/PhoneConfirmation';
 import CodeConfirm from './pages/CodeConfirm';
 import AllowNotification from './pages/AllowNotification';
+import AppLayout from './Layouts/AppLayout';
+import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
-      <PlanLayout>
-        <Route exact path="/" component={Welcome} />
-        <Route exact path="/invite" component={PhoneConfirmation} />
-        <Route exact path="/code_confirm" component={CodeConfirm} />
-        <Route exact path="/allow_notification" component={AllowNotification} />
-      </PlanLayout>
+      <Route
+        exact
+        path={[
+          '/',
+          '/invite',
+          '/get_username',
+          '/code_confirm',
+          '/allow_notification'
+        ]}
+      >
+        <PlanLayout>
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route exact path="/invite" component={PhoneConfirmation} />
+          <Route exact path="/code_confirm" component={CodeConfirm} />
+          <Route exact path="/allow_notification" component={AllowNotification} />
+          </Switch>
+        </PlanLayout>
+      </Route>
+      <Route exact path={['/home']}>
+        <AppLayout>
+          <Switch>
+            <Route exact path='/home' component={() => <Home />}></Route>
+          </Switch>
+        </AppLayout>
+      </Route>
     </BrowserRouter>
   );
 }
