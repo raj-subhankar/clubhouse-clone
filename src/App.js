795fdde492ddbeb5ca36c26d788883de4e0440ca
rj-subhankar <subhankar.rj@gmail.com>
2021-05-17 14:44:30 +0530

    Create AllowNotification

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import PlanLayout from './Layouts/PlanLayout';
 import { BrowserRouter, Route } from 'react-router-dom';
 import PhoneConfirmation from './pages/PhoneConfirmation';
 import CodeConfirm from './pages/CodeConfirm';
+import AllowNotification from './pages/AllowNotification';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" component={Welcome} />
         <Route exact path="/invite" component={PhoneConfirmation} />
         <Route exact path="/code_confirm" component={CodeConfirm} />
+        <Route exact path="/allow_notification" component={AllowNotification} />
       </PlanLayout>
     </BrowserRouter>
   );
---
+++ src/pages/AllowNotification.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import style from '../style/phoneConfirm.module.css';
+
+export default function AllowNotification() {
+    return(
+        <div className={style.phoneConfirmContainer}>
+            <div className='text-center'>
+                <h1 className='mb-4'>Last, important step!</h1>
+                <h1 className='mb-3'>Enable notification to know when people are talking</h1>
+                <div className={style.notificationContainer}>
+                    <div className="p-3">
+                        <h3>Clubhouse Would Like To Send You Notifications</h3>
+                        <p>Notifications may include alerts, sounds and icon badges</p>
+                    </div>
+                    <div className={style.action_btn}>
+                        <Link exact to='/home'>Don't Allow</Link>
+                        <Link exact to='/home'>Allow</Link>
+                    </div>
+                    <img src='/images/handIcon.svg' alt='' className={style.hand_icon}/>
+                </div>
+            </div>
+        </div>
+    )
+}
