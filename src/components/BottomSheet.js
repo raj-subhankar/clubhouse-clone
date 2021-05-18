2c4b0aa960eddcb4093e3c9aae770df1391d4aa9
rj-subhankar <subhankar.rj@gmail.com>
2021-05-18 19:52:20 +0530

    Implement Start Room with BottomSheet

---
+++ src/components/BottomSheet.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import style from '../style/bottomSheet.module.css';
+import SwipeableBottomSheet from 'react-swipeable-bottom-sheet';
+import StartRoom from './bottom_sheets/StartRoom';
+
+export default function BottomSheet(props) {
+    return (
+        <SwipeableBottomSheet
+            open={props.sheetVisible}
+            onChange={() => {
+                props.setSheetVisible(!props.sheetVisible)
+                props.setItemsVisible(true)
+            }}
+            fullScreen={props.sheetTitle == 'room detail' ? true : false}
+        >
+            <div className={style.BottomSheetContainer}
+                style={{backgroundColor: props.sheetTitle == 'profile' ? 'transparent' : ''}}
+            >
+                <StartRoom 
+                    setSheetCreateRoom={props.setSheetCreateRoom}
+                    setSheetVisible={(item) => {
+                        props.setSheetVisible(item);
+                        props.setItemsVisible(true);
+                    }}
+                />
+            </div>
+        </SwipeableBottomSheet>
+    )
+}
