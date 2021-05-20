e4e226609680222b89a85f05e478827266250fab
rj-subhankar <subhankar.rj@gmail.com>
2021-05-20 22:03:35 +0530

    Create Explore page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import Welcome from './pages/Welcome';
-import PlanLayout from './Layouts/PlanLayout';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import PhoneConfirmation from './pages/PhoneConfirmation';
-import CodeConfirm from './pages/CodeConfirm';
-import AllowNotification from './pages/AllowNotification';
-import AppLayout from './Layouts/AppLayout';
-import Home from './pages/Home';
+import logo from "./logo.svg";
+import "./App.css";
+import Welcome from "./pages/Welcome";
+import PlanLayout from "./Layouts/PlanLayout";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import PhoneConfirmation from "./pages/PhoneConfirmation";
+import CodeConfirm from "./pages/CodeConfirm";
+import AllowNotification from "./pages/AllowNotification";
+import AppLayout from "./Layouts/AppLayout";
+import Home from "./pages/Home";
+import Explore from "./pages/Explore";
 
 function App() {
   return (
@@ -15,26 +16,31 @@ function App() {
       <Route
         exact
         path={[
-          '/',
-          '/invite',
-          '/get_username',
-          '/code_confirm',
-          '/allow_notification'
+          "/",
+          "/invite",
+          "/get_username",
+          "/code_confirm",
+          "/allow_notification",
         ]}
       >
         <PlanLayout>
-        <Switch>
-          <Route exact path="/" component={Welcome} />
-          <Route exact path="/invite" component={PhoneConfirmation} />
-          <Route exact path="/code_confirm" component={CodeConfirm} />
-          <Route exact path="/allow_notification" component={AllowNotification} />
+          <Switch>
+            <Route exact path="/" component={Welcome} />
+            <Route exact path="/invite" component={PhoneConfirmation} />
+            <Route exact path="/code_confirm" component={CodeConfirm} />
+            <Route
+              exact
+              path="/allow_notification"
+              component={AllowNotification}
+            />
           </Switch>
         </PlanLayout>
       </Route>
-      <Route exact path={['/home']}>
+      <Route exact path={["/home", "/explore"]}>
         <AppLayout>
           <Switch>
-            <Route exact path='/home' component={() => <Home />}></Route>
+            <Route exact path="/home" component={() => <Home />}></Route>
+            <Route exact path="/explore" component={Explore} />
           </Switch>
         </AppLayout>
       </Route>
